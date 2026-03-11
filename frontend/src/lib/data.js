fd88152736df8a81917ae6bfbbaa2bd474e51a6b
Felipe Grolla Freitas <120500170+Grolla05@users.noreply.github.com>
2026-03-11 09:52:52 -0300

    Update data.js

--- frontend/src/lib/data.js
+++ frontend/src/lib/data.js
@@ -1,5 +1,5 @@
 export const SECTOR_DATA = {
-  P3: {
+  /*P3: {
     EMC: [["ENSAIOS DE EMC"]],
     RF: [["Bluetooth Low Energy", "Wi-Fi 2.4Ghz", "Wi-Fi 5Ghz", "DFS/TPC"]],
     INF: [["ENSAIOS DE INF"]],
@@ -13,14 +13,14 @@ export const SECTOR_DATA = {
   P5: {
     MED: [["ENSAIOS DE MED"]],
     ASE: [["ENSAIOS DE ASE"]],
-  },
+  },*/
   TESTE: {
     Geral: [["TESTE1", "TESTE2"]],
   },
 };
 
 export const LAYOUT_MAPPING = {
-  "Bluetooth Low Energy": "QkxFX2xheW91dC5kb2N4",
+  /*"Bluetooth Low Energy": "QkxFX2xheW91dC5kb2N4",
   "Wi-Fi 2.4Ghz": "V2ktRmkyLjRfbGF5b3V0LmRvY3g=",
   "Wi-Fi 5Ghz": "V2ktRmk1X2xheW91dC5kb2N4",
   "DFS/TPC": "REZTX1RQQ19sYXlvdXQuZG9jeA==",
@@ -31,7 +31,7 @@ export const LAYOUT_MAPPING = {
   "ENSAIOS DE DPC": "RFBDX2xheW91dC5kb2N4",
   "ENSAIOS DE CABL": "Q0FCTF9sYXlvdXQuZG9jeA==",
   "ENSAIOS DE MED": "TUVEX2xheW91dC5kb2N4",
-  "ENSAIOS DE ASE": "QVNFX2xheW91dC5kb2N4",
+  "ENSAIOS DE ASE": "QVNFX2xheW91dC5kb2N4",*/
   TESTE1: "VEVTVEUxX2xheW91dC5kb2N4",
   TESTE2: "VEVTVEUyX2xheW91dC5kb2N4",
 };
